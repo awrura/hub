733ffce8aacce60f82ebd072ce817c942cc22d30
twoics <second_vozhzhov@mail.ru>
2025-03-26 10:02:28 +0700

    feat: Добавил API для добавления матрицы к пользователю по секретному ключу

--- app/matrix/api/routes/user_matrix.py
+++ app/matrix/api/routes/user_matrix.py
@@ -4,7 +4,10 @@ from auth.util.access import user_from_token
 from dishka import FromDishka
 from dishka.integrations.fastapi import inject
 from fastapi import Depends
+from fastapi import status
+from fastapi.responses import Response
 from fastapi.routing import APIRouter
+from matrix.action.usecase.secret_key import UserMatrixSecretkeyUseCase
 from matrix.action.usecase.user_matrix import UserMatrixUseCase
 from matrix.api.schema.user_matrix import UserMatrixOutSchema
 from user.domain.user import User
@@ -24,3 +27,16 @@ async def my_matrix(
         UserMatrixOutSchema(name=matrix.name, height=matrix.height, width=matrix.width)
         for matrix in matrices
     ]
+
+
+@router.post('/add/{secret}')
+@inject
+async def add_user_matrix(
+    secret: str,
+    interactor: FromDishka[UserMatrixSecretkeyUseCase],
+    user: User = Depends(user_from_token),
+):
+    sucsess = await interactor.add_matrix_to_user(user, secret)
+    if sucsess:
+        return Response(status_code=status.HTTP_201_CREATED)
+    return Response(status_code=status.HTTP_400_BAD_REQUEST)
